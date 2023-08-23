d8f40749fc33a9210cdc4034cc02d7cb023d8691
Asum Victor <iamasum369@gmail.com>
2023-08-23 05:33:30 +0000

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,7 +1,7 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals VALUES ('Agumon', 'Feb 3rd, 2020', 0, TRUE, 10.23);
-INSERT INTO animals VALUES ('Gabumon', 'Nov 15th, 2018', 2, TRUE, 8);
-INSERT INTO animals VALUES ('Pikachu', 'Jan 7th, 2021', 1, FALSE, 15.04);
-INSERT INTO animals VALUES ('Devimon', 'May 12th, 2017', 5, TRUE, 11);
+INSERT INTO animals VALUES ('Agumon', '2020-02-03', 0, TRUE, 10.23);
+INSERT INTO animals VALUES ('Gabumon', '2018-11-15', 2, TRUE, 8);
+INSERT INTO animals VALUES ('Pikachu', '2021-01-07', 1, FALSE, 15.04);
+INSERT INTO animals VALUES ('Devimon', '2017-05-12', 5, TRUE, 11);
 
