94f7f6edfa87e10c82e67a4e74e9390c68c0cb26
Asum Victor <iamasum369@gmail.com>
2023-08-23 05:20:40 +0000

    Added Auto increament property

--- schema.sql
+++ schema.sql
@@ -1,11 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT PRIMARY KEY NOT NULL, 
+    id INT  PRIMARY KEY  NOT NULL AUTO_INCREMENT, 
     name VARCHAR(100) NOT NULL, 
     date_of_birth DATE NOT NULL, 
     escape_attempt INT, 
     neutered BOOLEAN, 
     weight_kg DECIMAL NOT NULL
-
 );
