a73631039b1ee97bb7c2307908ddb185a0f4b783
Asum Victor <iamasum369@gmail.com>
2023-08-23 05:29:57 +0000

    Inserted data in the animals table

--- data.sql
+++ data.sql
@@ -1,5 +1,7 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals VALUES ('Agumon', 'Feb 3rd, 2020', 0, TRUE, 10.23);
+INSERT INTO animals VALUES ('Gabumon', 'Nov 15th, 2018', 2, TRUE, 8);
+INSERT INTO animals VALUES ('Pikachu', 'Jan 7th, 2021', 1, FALSE, 15.04);
+INSERT INTO animals VALUES ('Devimon', 'May 12th, 2017', 5, TRUE, 11);
+
