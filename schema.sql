84dd05b8879766410e6f0b3bad60a7765dfd49d1
Asum Victor <iamasum369@gmail.com>
2023-08-23 07:19:42 +0000

    Initial commits

--- schema.sql
+++ schema.sql
@@ -1,10 +1,5 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-     id INT GENERATED ALWAYS AS IDENTITY, 
-    name VARCHAR(100) NOT NULL, 
-    date_of_birth DATE NOT NULL, 
-    escape_attempt INT, 
-    neutered BOOLEAN, 
-    weight_kg DECIMAL NOT NULL
+    name varchar(100)
 );
