fa93cfdb49d9642029265b19682723596ad75560
Asum Victor <iamasum369@gmail.com>
2023-08-24 05:53:38 +0000

    Added queries for transactin

--- data.sql
+++ data.sql
@@ -5,3 +5,12 @@ INSERT INTO animals VALUES ('Gabumon', '2018-11-15', 2, TRUE, 8);
 INSERT INTO animals VALUES ('Pikachu', '2021-01-07', 1, FALSE, 15.04);
 INSERT INTO animals VALUES ('Devimon', '2017-05-12', 5, TRUE, 11);
 
+
+INSERT INTO animals VALUES ('Charmander', '2020-02-08', 0, false, -11);
+INSERT INTO animals VALUES ('Plantmon', '2021-11-15', 2, true, -5.7);
+INSERT INTO animals VALUES ('Squirtle', '1993-04-02', 3, false, -12.13);
+INSERT INTO animals VALUES ('Angemon', '2005-06-12', 1, true, -45);
+INSERT INTO animals VALUES ('Boarmon', '2005-06-07', 7, true, 20.4);
+INSERT INTO animals VALUES ('Blossom', '1998-10-13', 3, true, 17);
+INSERT INTO animals VALUES ('Ditto', '2022-05-14', 4, true, 22);
+
